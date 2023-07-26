da8bba54b504414b8deec629cdf87ebd63fbc959
Vinicius Aderaldo <138501694+viniciusaderaldo@users.noreply.github.com>
2023-07-26 13:07:30 -0300

    Primeira versão

---
+++ scripts/script.js
@@ -0,0 +1,92 @@
+const jogadorDaVez = document.querySelector(".jogadorDaVez");
+const espacos = document.querySelectorAll("[data-espaco]");
+const campoMensagemFinal = document.querySelector(".mensagemFinal");
+
+const wins = [
+    ["0","1","2"],
+    ["0","3","6"],
+    ["0","4","8"],
+    ["1","4","7"],
+    ["2","5","8"],
+    ["2","4","6"],
+    ["3","4","5"],
+    ["6","7","8"]
+];
+
+var playerX = [];
+var playerO = [];
+let jogadaId;
+
+var player = "X";
+var espacoOcupado = 0;
+semVencedor = true;
+espacos.forEach(espaco => {
+    espaco.addEventListener('click', () => {
+        jogadaId = espaco.id;
+        espacoOcupado++;
+        espaco.innerHTML = player;
+        if(player === "X"){
+            playerX.push(jogadaId);
+            verificaVencedor(playerX)
+            venceu(semVencedor, player);
+            player = "O"
+        }else{
+            playerO.push(jogadaId);
+            verificaVencedor(playerO)
+            venceu(semVencedor, player);
+            player = "X"
+        }
+        jogadorDaVez.innerHTML = `É a vez do jogador ${player}`;
+    });
+});
+
+
+function verificaVencedor(jogadas){
+    let jogadaEncontrada;
+    wins.every(win => {
+        let soma = 0;
+        for(let jogada = 0; jogada < jogadas.length; jogada++){
+            jogadaEncontrada = win.indexOf(jogadas[jogada]);
+            if(jogadaEncontrada != -1){
+                soma++
+            }
+        }
+        if(soma == 3){
+            semVencedor = false;
+            return semVencedor;
+        } else if (soma != 3 && espacoOcupado == 9){
+            campoMensagemFinal.innerHTML = `
+                <p>Deu <em>velha</em></p>
+                <button type="reset" id="jogarNovamente">jogar novamente</button>
+                <button type="button" id="finalizar">finalizar</button>
+            `;
+            funcionalidadeBotoes();
+        } else{
+            semVencedor = true;
+            return semVencedor;
+        }
+    });
+}
+
+function venceu(continuar, jogador){
+    if(continuar == false){
+        campoMensagemFinal.innerHTML = `
+            <p>O <em>jogador ${jogador}</em> venceu!</p>
+            <button type="reset" id="jogarNovamente">jogar novamente</button>
+            <button type="button" id="finalizar">finalizar</button>
+        `;
+        funcionalidadeBotoes();
+    }
+}
+
+function funcionalidadeBotoes(){
+    const botaoJogarNovamente = document.getElementById("jogarNovamente");
+    const botaoFinalizar = document.getElementById("finalizar");
+
+    botaoJogarNovamente.addEventListener('click', () => {
+        window.location.href = "index.html";
+    });
+    botaoFinalizar.addEventListener('click', () => {
+        window.location.href = "pages/despedida.html"
+    });
+}
